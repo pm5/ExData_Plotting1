5e416c13c65010651ad8aa02561c399ab3c16b7f
Pomin Wu <pomin5@gmail.com>
2014-08-09 18:00:32 +0800

    add plot 1

---
+++ plot1.R
@@ -0,0 +1,4 @@
+data <- read.csv("power.txt", sep = ";", colClasses = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+pdf('plot1.png')
+hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
